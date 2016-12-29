3a40e8bb47d5f97e6d613c8dcdb4326948db89ed
yanzhipeng <307323665@qq.com>
2016-12-29 15:59:14 +0800

    s

--- test/src/test/Hello.java
+++ test/src/test/Hello.java
@@ -3,7 +3,10 @@ package test;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("git    ");
+		System.out.println("git  "
+				+ ""
+				+ ""
+				+ "  ");
 	}
 
 }
